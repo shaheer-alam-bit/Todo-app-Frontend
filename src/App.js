07a05d6d5eb143da61fdf31004bf6b8eeb97c3ef
Shaheer Alam <134544657+shaheer-alam-bit@users.noreply.github.com>
2025-06-11 02:08:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  const url = 'http://localhost:5000';
+  const url = 'http://host.docker.internal:5000';
 
   const fetchTodos = async () => {
     const response = await fetch(`${url}/api/todos`);
